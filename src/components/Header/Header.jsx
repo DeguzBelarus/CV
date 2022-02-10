0d4305680aea0f471feb807623e64929d7902188
Anton <deguz@mail.ru>
2022-02-11 01:39:05 +0300

    visuals

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,8 +11,22 @@ const Header = () => {
       <div className="round"></div>
       <img src={avatar} className="avatar-image" alt="avatar picture" />
       <div className="headers">
-        <h1>Anton Dektyarev</h1>
-        <h3>Frontend developer</h3>
+        <span className="letter l1">A</span>
+        <span className="letter l2">n</span>
+        <span className="letter l3">t</span>
+        <span className="letter l4">o</span>
+        <span className="letter l5">n</span>
+        <span className="letter l6">D</span>
+        <span className="letter l7">e</span>
+        <span className="letter l8">k</span>
+        <span className="letter l9">t</span>
+        <span className="letter l10">y</span>
+        <span className="letter l11">a</span>
+        <span className="letter l12">r</span>
+        <span className="letter l13">e</span>
+        <span className="letter l14">v</span>
+
+        <h3 className="profession">Frontend developer</h3>
       </div>
       <div className="socials">
         <div className="socials-item">
