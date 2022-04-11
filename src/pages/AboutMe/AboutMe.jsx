e52d1799a6701311a65ab935736ac6be7295d33c
Anton <deguz@mail.ru>
2022-04-11 11:48:28 +0300

    about me info was updated

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -13,13 +13,13 @@ const AboutMe = () => {
       <p className="about-paragraph">
         I have chosen Front End because I prefer to work with the visual
         interface and the program/style code, not only with the program code.
-        From 2006 till 2008 I made a website about bodybuilding. This site only
-        used HTML and some JavaScript functionality.
+        From 2006 till 2008 I made a simple website about bodybuilding. This
+        site only used HTML and some JavaScript functionality.
       </p>
       <p className="about-paragraph">
-        I have six+ months of non-commercial experience in Front-End Development.
-        Also, I got to know some Back-end basics such as Node.js, Express,
-        MongoDB and Socket.io. I have some projects that deployed to
+        I have about 7 months of non-commercial practice in Front-End
+        Development. Also, I got to know some Back-end basics such as Node.js,
+        Express, MongoDB and Socket.io. I have some projects that deployed to
         GitHub and Heroku.
       </p>
       <p className="about-paragraph">
